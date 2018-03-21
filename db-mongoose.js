a318ad101345f04a96dd2b3a415e7cb2a0e3749b
Nicci Williams <nicci@nicciwill.com>
2018-03-21 15:12:10 -0400

    New heroku deploy

--- db-mongoose.js
+++ db-mongoose.js
@@ -7,7 +7,7 @@ const {DATABASE_URL} = require('./config');
 
 function dbConnect(url = DATABASE_URL) {
   return mongoose
-    .connect(url, { useMongoClient: true })  
+    .connect(url)  
     .catch(err => {
       console.error('Mongoose failed to connect');
       console.error(err);
